e309c657be3cb1c877b20b22484fca35572a9667
Julian C. Dunn <jdunn@chef.io>
2015-10-31 18:28:24 +0000

    Update email address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'yum-oracle'
 maintainer 'Chef Software, Inc.'
-maintainer_email 'jdunn@getchef.com'
+maintainer_email 'jdunn@chef.io'
 license 'Apache 2.0'
 description 'Configures yum repos on an Oracle Linux system'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
