9b97f9069b49a534e67c388f857cd207e8da160a
Goran Stankovski <gstankovski@limepoint.com>
2016-02-03 22:36:08 +1100

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jdunn@chef.io'
 license 'Apache 2.0'
 description 'Configures yum repos on an Oracle Linux system'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '1.0.2'
 
 depends 'yum', '~> 3.0'
 supports 'oracle'
