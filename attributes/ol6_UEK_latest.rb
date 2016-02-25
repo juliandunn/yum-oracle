e42682a47d648b2a0b71dd56e8d9bf732cb206a4
212050466 <josh.schneider@ge.com>
2016-02-25 11:18:40 -0600

    Updated to use `make_cache` parameter for yum resource

--- attributes/ol6_UEK_latest.rb
+++ attributes/ol6_UEK_latest.rb
@@ -2,6 +2,7 @@ default['yum']['ol6_UEK_latest']['repositoryid'] = 'ol6_UEK_latest'
 default['yum']['ol6_UEK_latest']['description'] = 'Latest Unbreakable Enterprise Kernel for Oracle Linux $releasever ($basearch)'
 default['yum']['ol6_UEK_latest']['baseurl'] = 'http://public-yum.oracle.com/repo/OracleLinux/OL6/UEK/latest/$basearch/'
 default['yum']['ol6_UEK_latest']['enabled'] = true
+default['yum']['ol6_UEK_latest']['make_cache'] = true
 default['yum']['ol6_UEK_latest']['managed'] = true
 default['yum']['ol6_UEK_latest']['metadata_expire'] = '7d'
 default['yum']['ol6_UEK_latest']['gpgcheck'] = true
